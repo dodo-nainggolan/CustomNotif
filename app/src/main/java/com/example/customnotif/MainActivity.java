f202ba3401cd2c6fdeac08e7ad9f949f917aea36
dodo-nainggolan <valdo.henschel@gmail.com>
2019-11-11 17:25:26 +0700

    #finished

--- app/src/main/java/com/example/customnotif/MainActivity.java
+++ app/src/main/java/com/example/customnotif/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.customnotif;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonNotif = findViewById(R.id.button_show_notification);
+        buttonNotif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, NotificationService.class));
+            }
+        });
+
     }
 }
